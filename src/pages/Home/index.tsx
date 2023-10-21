6d753f82ef0ba75c48615e1397e0373c3757e270
klarsongs <klarsongs@gmail.com>
2023-10-21 15:48:13 +0200

    Add react hook form for home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,16 @@
+import { Button, Row } from 'antd';
+import { Link } from 'react-router-dom';
+import { StyledCol } from './styled';
+
 export const HomePage = () => {
-  return <div>home page</div>;
+  return (
+    <Row justify='center'>
+      <StyledCol span={12}>Obrazek tada</StyledCol>
+      <StyledCol span={12}>
+        <Link to='/organizacja'>
+          <Button type='primary'>Wejdź</Button>
+        </Link>
+      </StyledCol>
+    </Row>
+  );
 };
