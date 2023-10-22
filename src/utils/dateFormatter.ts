a411116aad341eb952d2d388bbadfdee2c6253bf
klarsongs <klarsongs@gmail.com>
2023-10-22 07:06:59 +0200

    Fixes for form

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -17,8 +17,6 @@ export function formatDateToPL(date: Date) {
   const day = date.getDate();
   const month = months[date.getMonth()];
   const year = date.getFullYear();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
 
-  return `${day} ${month} ${year}, ${hours}:${minutes}`;
+  return `${day} ${month} ${year}`;
 }
